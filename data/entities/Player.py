8c329d5e8d3988ce22bd3f65549062cbb9476b03
Niccolò Bertozzi <116648770+NiccoloBert0zzi@users.noreply.github.com>
2024-08-10 09:36:13 +0200

    add heart png and fix entities class

--- data/entities/Player.py
+++ data/entities/Player.py
@@ -1,21 +1,22 @@
 import pygame
-from Laser import Laser
+from data.entities import Laser
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, constraint, speed):
+    def __init__(self, pos, constraint, speed, margin_x):
         super().__init__()
-        self.image = pygame.image.load('../graphics/player.png').convert_alpha()
+        self.image = pygame.image.load('ui/graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom=pos)
         self.speed = speed
         self.max_x_constraint = constraint
+        self.margin_x = margin_x
         self.ready = True
         self.laser_time = 0
         self.laser_cooldown = 600
 
         self.lasers = pygame.sprite.Group()
 
-        self.laser_sound = pygame.mixer.Sound('../audio/laser.wav')
+        self.laser_sound = pygame.mixer.Sound('audio/laser.wav')
         self.laser_sound.set_volume(0.5)
 
     def get_input(self):
@@ -39,13 +40,13 @@ class Player(pygame.sprite.Sprite):
                 self.ready = True
 
     def constraint(self):
-        if self.rect.left <= 0:
-            self.rect.left = 0
-        if self.rect.right >= self.max_x_constraint:
-            self.rect.right = self.max_x_constraint
+        if self.rect.left <= self.margin_x:
+            self.rect.left = self.margin_x
+        if self.rect.right >= self.max_x_constraint + self.margin_x:
+            self.rect.right = self.max_x_constraint + self.margin_x
 
     def shoot_laser(self):
-        self.lasers.add(Laser(self.rect.center, -8, self.rect.bottom))
+        self.lasers.add(Laser.Laser(self.rect.center, -8, self.rect.bottom))
 
     def update(self):
         self.get_input()
